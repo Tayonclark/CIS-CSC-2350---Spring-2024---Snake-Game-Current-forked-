ca364ebd4f00cede4d7ce17a0869e5cbd3924056
Tayon Clark <tclark1@lions.molloy.edu>
2024-05-22 22:55:21 +0000

    VI

--- src/Snake.ts
+++ src/Snake.ts
@@ -30,11 +30,11 @@ class SnakeController {
   }
 
   get worldWidth() {
-    return this.snakeWorld.width;
+    return this.snakeWorld.allSnakes;
   }
 
   get worldHeight() {
-    return this.snakeWorld.height;
+    return this.snakeWorld.allViews;
   }
 }
 
@@ -56,7 +56,8 @@ class AvoidWallsPlayer extends Player {
   makeTurn(): void {
     const snakeDirection = this.sc.snakeDirection;
     const snakePosition = this.sc.snakePosition;
-    const worldHeight = this.sc.worldHeight;
+    const worldHeight = 30;
+
     if (
       snakeDirection === "left" &&
       snakePosition.x === 0 &&
@@ -69,6 +70,7 @@ class AvoidWallsPlayer extends Player {
 
 interface IWorldView {
   display(WorldModel: WorldModel): void;
+  update(model: WorldModel): void;
 }
 
 class CanvasWorldView implements IWorldView {
@@ -84,11 +86,30 @@ class CanvasWorldView implements IWorldView {
   }
 
   display(WorldModel: WorldModel): void {
-    this.worldCanvas.width = WorldModel.width * this.scalingFactor;
-    this.worldCanvas.height = WorldModel.height * this.scalingFactor;
+    this.worldCanvas.width = 30 * this.scalingFactor;
+    this.worldCanvas.height = 40 * this.scalingFactor;
     this.context.fillStyle = "orange";
-    this.context.fillRect(0, 0, this.scalingFactor, this.scalingFactor);
+    this.context.fillRect(
+      0,
+      0,
+      this.worldCanvas.width,
+      this.worldCanvas.height,
+    );
+
+    for (const snake of WorldModel.allSnakes) {
+      for (const part of snake.currentParts) {
+        this.context.fillStyle = "green";
+        this.context.fillRect(
+          part.x * this.scalingFactor,
+          part.y * this.scalingFactor,
+          this.scalingFactor,
+          this.scalingFactor,
+        );
+      }
+    }
   }
+
+  update(model: WorldModel): void {}
 }
 
 class Point {
@@ -107,29 +128,47 @@ class Point {
   get y(): number {
     return this.ycoord;
   }
+
+  equals(p: Point): boolean {
+    return this.x === p.x && this.y === p.y;
+  }
 }
 
 type Direction = "left" | "right" | "up" | "down";
 
 class Snake {
-  private currentPosition: Point;
+  public currentParts: Point[];
   public currentDirection: Direction; // changed from private to protected
 
-  constructor() {
-    this.currentPosition = new Point(0, 0);
+  constructor(startPosition: Point, size: number) {
+    this.currentParts = [startPosition];
+    for (let i = 1; i < size; i++) {
+      const tailPart = new Point(startPosition.x, startPosition.y + i);
+      this.currentParts.push(tailPart);
+    }
     this.currentDirection = "up";
   }
 
   move(distance: number) {
-    let NX = this.currentPosition.x;
-    let NY = this.currentPosition.y;
+    for (let i = this.currentParts.length - 1; i > 0; i--) {
+      this.currentParts[i] = this.currentParts[i - 1];
+    }
 
-    if (this.currentDirection === "up") NY += distance;
-    else if (this.currentDirection === "right") NX -= distance;
-    else if (this.currentDirection === "down") NY += distance;
-    else if (this.currentDirection === "left") NX -= distance;
+    const head = this.currentParts[0];
+    let newHead: Point;
 
-    this.currentPosition = new Point(NX, NY);
+    if (this.currentDirection === "up") {
+      newHead = new Point(head.x, head.y - distance);
+    } else if (this.currentDirection === "right") {
+      newHead = new Point(head.x + distance, head.y);
+    } else if (this.currentDirection === "down") {
+      newHead = new Point(head.x, head.y + distance);
+    } else if (this.currentDirection === "left") {
+      newHead = new Point(head.x - distance, head.y);
+    } else {
+      throw new Error("Invalid direction");
+    }
+    this.currentParts[0] = newHead;
   }
 
   turnLeft() {
@@ -147,35 +186,66 @@ class Snake {
   }
 
   get Position(): Point {
-    return this.currentPosition;
+    return this.currentParts[0];
+  }
+
+  collidesWith(otherSnake: Snake): boolean {
+    const head = this.currentParts[0];
+    const otherParts = otherSnake.currentParts;
+    return otherParts.some((part) => part.equals(head));
+  }
+
+  didCollide(s: Snake): boolean {
+    const head = this.currentParts[0];
+    const otherParts = this.currentParts.slice(1);
+    return otherParts.some((part) => part.equals(head));
   }
 }
 
 class WorldModel {
-  private snake: Snake;
-  width: number;
-  height: number;
-  worldView: IWorldView | null = null;
+  public allSnakes: Snake[] = [];
+  public allViews: IWorldView[] = [];
 
-  constructor(snake: Snake, width: number, height: number) {
-    this.snake = snake;
-    this.width = width;
-    this.height = height;
+  constructor() {}
+
+  addSnake(s: Snake): void {
+    this.allSnakes.push(s);
+  }
+
+  addView(v: IWorldView): void {
+    this.allViews.push(v);
   }
 
   update(steps: number): void {
-    this.snake.move(steps);
-    if (this.worldView) {
-      this.worldView.display(this);
+    const collidedSnakes: Snake[] = [];
+    for (let i = 0; i < this.allSnakes.length; i++) {
+      const snakeA = this.allSnakes[i];
+      for (let j = i + 1; j < this.allSnakes.length; j++) {
+        const snakeB = this.allSnakes[j];
+        if (snakeA.collidesWith(snakeB) && !collidedSnakes.includes(snakeA)) {
+          collidedSnakes.push(snakeA);
+        }
+      }
+    }
+
+    for (const collidedSnake of collidedSnakes) {
+      const index = this.allSnakes.indexOf(collidedSnake);
+      if (index !== -1) {
+        this.allSnakes.splice(index, 1);
+      }
+    }
+
+    for (const snake of this.allSnakes) {
+      snake.move(steps);
+    }
+
+    for (const view of this.allViews) {
+      view.update(this);
     }
   }
 
-  setView(view: IWorldView): void {
-    this.worldView = view;
-  }
-
-  get snakeInstance(): Snake {
-    return this.snake;
+  get snakes(): Snake[] {
+    return this.allSnakes;
   }
 }
 
